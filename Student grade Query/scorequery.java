da854d6213606a2e3df2e0b26be889c86cdc6523
Spiderman_git <118711195+pengchao2022@users.noreply.github.com>
2024-07-27 23:08:24 +0800

    Update scorequery.java

--- Student grade Query/scorequery.java
+++ Student grade Query/scorequery.java
@@ -1,6 +1,4 @@
 
-
-
 // this program is to query grade for students
 // this program is written by pengchao in Shanghai
 
@@ -9,19 +7,22 @@ import java.util.Scanner;
 public class gradequery {
     public static void main(String[] args) {
 
-        final int PASS = 60;
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Please enter your grade here:");
-        int score = scan.nextInt();
-        System.out.println("your score is: " + score);
+       
+        while (true) {
+            final int PASS = 60;
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Please enter your grade here:");
+            int score = scan.nextInt();
+            System.out.println("your score is: " + score);
 
-        if(score < PASS){
-            System.out.println("Sorry,You are not qualified!");
+            if(score < PASS){
+                System.out.println("Sorry,You are not qualified!");
 
-        }
-        else{
-            System.out.println("greetings, You score is OKay");
-            System.out.println("Bye Bye");
+            }
+            else{
+                System.out.println("greetings, You score is OKay");
+                System.out.println("Bye Bye");
+            }
         }
         
     }
